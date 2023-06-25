5a361f258aa3cad987c13b4c1f35a905da36b18f
早起喝燕麦 <zhuowangyi@163.com>
2023-06-25 17:04:48 +0800

    element-plus 使用

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 const pinia = createPinia();
 
-createApp(App).use(pinia).use(router).mount("#app");
+createApp(App).use(pinia).use(router).use(ElementPlus).mount("#app");

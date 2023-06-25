685b751983d4244d592ed3424fcf017db04e5709
早起喝燕麦 <zhuowangyi@163.com>
2023-06-25 17:47:30 +0800

    layout基础搭建

---
+++ src/components/layout/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <sidebar class="sidebar-container" />
+    <div class="main-container">
+      <div>
+        <navbar />
+      </div>
+      <app-main />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { Sidebar, Navbar, AppMain } from "./index.js";
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
-
+import Layout from "@/components/layout/index.vue";
 import Home from "../views/home/index.vue";
 import About from "../views/about/index.vue";
 
 const routes = [
-  { path: "/", component: Home },
+  {
+    path: "/",
+    component: Layout,
+    redirect: "/home",
+    meta: { title: "概览" },
+    name: "home",
+    children: [
+      {
+        path: "home",
+        component: Home,
+        name: "概览",
+      },
+    ],
+  },
   { path: "/about", component: About },
 ];
 

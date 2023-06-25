a68a3620149e919010139b2ecf4ac117214ea31f
早起喝燕麦 <zhuowangyi@163.com>
2023-06-25 20:09:13 +0800

    init

--- src/components/layout/index.vue
+++ src/components/layout/index.vue
@@ -13,6 +13,12 @@
 <script setup>
 import { Sidebar, Navbar, AppMain } from "./index.js";
 import { mapState } from "pinia";
-import {  useAppStore } from "@/store/pinia";
+import { useAppStore } from "@/store/pinia";
 import { computed } from "vue";
+
+let sidebarShow = ref(false);
+
+sidebarShow.value = computed((state) => {
+  return state.sidebar;
+});
 </script>

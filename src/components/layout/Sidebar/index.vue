934099c77d1bee6f22e0d2ba82ccd2f1469edabe
早起喝燕麦 <zhuowangyi@163.com>
2023-06-27 14:18:58 +0800

    设置菜单导航

--- src/components/layout/Sidebar/index.vue
+++ src/components/layout/Sidebar/index.vue
@@ -1,3 +1,47 @@
 <template>
-    <div> sidebar</div>
+  <div>
+    <div>
+      <el-button @click="handleToggle" type="primary">Primary</el-button>
+    </div>
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      :collapse="isCollapse"
+      :collapse-transition="true"
+    >
+      <Menu-item
+        v-for="route in permission_routes"
+        :key="route.id"
+        :item="route"
+      />
+    </el-menu>
+  </div>
 </template>
+
+<script setup>
+import { ref, computed } from "vue";
+import { usePermissionStore } from "@/store";
+
+import MenuItem from "./menuItem.vue";
+
+const isCollapse = ref(false);
+
+let permissionStore = usePermissionStore();
+
+let permission_routes = computed(() => {
+  return permissionStore.permission_routes;
+});
+function handleToggle() {
+  isCollapse.value = !isCollapse.value;
+}
+</script>
+
+<style>
+.sidebar-container {
+  background: #fff;
+}
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+</style>
---
+++ src/components/layout/Sidebar/menuItem.vue
@@ -0,0 +1,64 @@
+<template>
+  <el-menu-item
+    v-if="hasOneShowingChild(item.children, item)"
+    :index="item.newLink"
+    @click="handleActiveRouter(item)"
+  >
+    <el-icon v-if="item.icon"><setting /></el-icon>
+    <template #title>{{ item.name }}</template>
+  </el-menu-item>
+
+  <el-sub-menu v-else :index="item.id">
+    <template #title>
+      <el-icon><location /></el-icon>
+      <span>{{ item.name }}</span>
+    </template>
+    <Menu-item v-for="child in item.children" :key="child.id" :item="child" />
+  </el-sub-menu>
+</template>
+
+<script setup>
+import { useRouter, useRoute } from "vue-router";
+
+import { computed, defineProps } from "vue";
+import {
+  Document,
+  Menu as IconMenu,
+  Location,
+  Setting,
+} from "@element-plus/icons-vue";
+const route = useRoute();
+const router = useRouter();
+
+defineProps({
+  item: {
+    type: Object,
+    default(rawProps) {
+      return {};
+    },
+  },
+});
+function hasOneShowingChild(children = [], parent) {
+  if (children.length > 0) {
+    return false;
+  } else {
+  }
+  return true;
+}
+
+function handleActiveRouter(item) {
+  const path = route.path;
+  let { newLink } = item;
+  if (path == newLink) {
+    return;
+  }
+  router.push({ path: newLink || "/" });
+}
+</script>
+
+<style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+</style>
